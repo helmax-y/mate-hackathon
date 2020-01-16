50e6618a3be2c14cd4b0d6cd6928071ea58de98a
helmax-y <max_rad@ukr.net>
2020-01-16 15:52:08 +0200

    Updates

---
+++ precache-manifest.c7a0264157be8d5cab253745f787c665.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659383c5ac56f795e4fa6f78cf9ea749",
+    "url": "/mate-hackathon/index.html"
+  },
+  {
+    "revision": "d774c207f833d6b7b026",
+    "url": "/mate-hackathon/static/js/2.d238544e.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/mate-hackathon/static/js/2.d238544e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "799a5d2c19ba1fb3cae5",
+    "url": "/mate-hackathon/static/js/main.1f6e887f.chunk.js"
+  },
+  {
+    "revision": "c2b2de1d33c5478fbee5",
+    "url": "/mate-hackathon/static/js/runtime-main.d05eee6b.js"
+  }
+]);

0206fddd77dfa2cf34b9700d2edd97098dd7a794
Anna Gerdii <annagerdiy@gmail.com>
2020-01-18 22:31:49 +0200

    api

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { getTodosFromServer } from "./api";
 
 import Month from './components/Month';
 import Day from './components/Day';
 
 const App = () => {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const todos = await getTodosFromServer();
+      setTodos(todos);
+    }
+    fetchData();
+  }, []);
+
+  console.log(todos);
+
   return (
     <Switch>
       <Route path="/" exact component={Month} />

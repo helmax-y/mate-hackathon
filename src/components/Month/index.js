44eeadd1272374fc6ac7f11e39efff3345f97895
helmax-y <max_rad@ukr.net>
2020-01-18 17:07:51 +0200

    Completed Month

--- src/components/Month/index.js
+++ src/components/Month/index.js
@@ -1,35 +1,104 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import './month.scss';
-import { selectDates } from '../../store';
+import React, { useState } from 'react';
+import './table.scss'
 
-const Month = (dates) => {
-  console.log(dates);
-  console.log(dates.dates);
-  
+const date = new Date();
+
+const getLocalDay = date => {
+  let day = date.getDay();
+
+  if (day === 0) {
+    day = 7;
+  }
+
+  return day;
+};
+
+
+
+const Month = () => {
+  const [year, setYear] = useState(date.getFullYear());
+  const [month, setMonth] = useState(date.getMonth() + 1);
+
+  const monthLength = new Date(year, month, 0).getDate();
+  const monthStartDay = getLocalDay(new Date(year, month - 1));
+  const startArr = Array(monthStartDay - 1);
+  const monthEndDay = getLocalDay(new Date(year, month - 1, monthLength));
+  const endArr = Array(7 - monthEndDay);
+  const daysArr = ['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU'];
+  const middleArr = Array.from(Array(monthLength).keys()).map(date => date + 1);
+  const  datesArr = [...startArr, ...middleArr, ...endArr];
+
+  const decrease = () => {
+    if (month === 1) {
+      setYear(year - 1);
+      setMonth(12);
+    } else {
+      setMonth(month - 1);
+    }
+  };
+
+  const increase = () => {
+    if (month === 12) {
+      setYear(year + 1);
+      setMonth(1);
+    } else {
+      setMonth(month + 1);
+    }
+  };
 
   return (
     <>
-      <h2 className="title">January 2020</h2>
-      
-      <article className="month">
-        {['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU'].map(weekday => (
-          <section className="month__week-day">
-            {weekday}
-          </section>
-        ))}
-        {dates.dates.map(({ date }) => (
-          <section className="month__day">
-            {date}
-          </section>
-        ))}
-      </article>
+      <button
+        type="button"
+        onClick={decrease}
+      >
+        Prev
+      </button>
+
+      <h2 className="title">{month} /{year}</h2>
+
+      <button 
+        type="button"
+        onClick={increase}
+      >
+        Next
+      </button>
+
+      <table className="table">
+        <thead>
+          <tr>
+            {daysArr.map(day => <th className="table__th">{day}</th>)}
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            {datesArr.slice(0,7)
+              .map(date => <td className="table__td">{date}</td>)}
+          </tr>
+          <tr>
+            {datesArr.slice(7,14)
+              .map(date => <td className="table__td">{date}</td>)}
+          </tr>
+          <tr>
+            {datesArr.slice(14,21)
+              .map(date => <td className="table__td">{date}</td>)}
+          </tr>
+          <tr>
+            {datesArr.slice(21,28)
+              .map(date => <td className="table__td">{date}</td>)}
+          </tr>
+          <tr>
+            {datesArr.slice(28, 35)
+              .map(date => <td className="table__td">{date}</td>)}
+          </tr>
+          <tr>
+            {datesArr.slice(35)
+              .map(date => <td className="table__td">{date}</td>)}
+          </tr>
+        </tbody>
+      </table>
     </>
-  )
+  );
 };
 
-const mapStateToProps = state => ({
-  dates: selectDates(state),
-});
-
-export default connect(mapStateToProps)(Month);
+export default Month;

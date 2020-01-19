73cebc12cece72f2c8baa65f7e3ac38990a69c74
helmax-y <max_rad@ukr.net>
2020-01-19 05:01:17 +0200

    Added semi marks

--- src/components/Day/index.js
+++ src/components/Day/index.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { useLocation } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import './day.scss';
 import monthesAccord from '../monthesAccord';
-import Input  from '../Input'
+import Input  from '../Input';
+import { getTodos } from '../../store';
 
 const hours = [...Array(24).keys()];
 
 const Day = () => {
+  console.log(getTodos);
+  
+  const todos = useSelector(getTodos);
   const location = useLocation();
   const date = location.pathname.slice(1).split('-');
+
+  const isThereEvent = todos.find(todo => todo.date === location.pathname.slice(1));
+  const isImportant = isThereEvent && isThereEvent.isImportant;
 
   return (
     <div className="day">
@@ -21,6 +29,20 @@ const Day = () => {
           <div className="day__hour">
             <span className="day__hour-text">{`${hour}:00`}</span>
             <Input hour={hour} />
+
+            <label>Important
+              <input 
+                type="checkbox" 
+                checked={isImportant}
+              />
+            </label>
+
+            <select
+              // onChange={}
+            >
+              <option value="">work</option>
+              <option value="">personal</option>
+            </select>
           </div>
         ))}
       </div>
